13b4044bb79b2a68b8f9ffa4149e2c177b4a7507
SevenCSJ88 <giangatong19990999@gmail.com>
2024-03-23 00:12:15 +0700

    first commit

---
+++ src/pages/Account.jsx
@@ -0,0 +1,38 @@
+import Slider from "../Components/Slider";
+import FooterList from "../Components/FooterList";
+import { Breadcrumb } from "antd";
+import UpdateProfileForm from "../Components/UpdateProfile/UpdateProfileForm";
+const Account = () => {
+  return (
+    <>
+      <div>
+        <Slider />
+      </div>
+      <div className="content">
+        <div className="title-home">
+          <Breadcrumb
+            items={[{ title: "Trang chủ" }, { title: "Tài Khoản" }]}
+          />
+        </div>
+
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <div className="col-md-6 offset-md-3">
+              <div className="card">
+                <div className="card-body">
+                  <h2 className="card-title text-center mb-4">
+                    Cập nhật thông tin cá nhân
+                  </h2>
+                  <UpdateProfileForm />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <FooterList />
+    </>
+  );
+};
+
+export default Account;
